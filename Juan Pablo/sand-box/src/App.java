d3c1f975487fa79ac58bc11728765b69c2147bd7
Juan Pablo Dias de Souza Nóbrega <juanobrega@dinfor.uniceub>
2025-02-13 20:48:26 -0300

    uma alteração

--- Juan Pablo/sand-box/src/App.java
+++ Juan Pablo/sand-box/src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Olá Orientação à Objetos");
+        System.out.println("Olá Mundo");
     }
 }
