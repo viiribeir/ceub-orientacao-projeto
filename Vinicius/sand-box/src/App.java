7508afa6c63a77591dca99be2204e24e54d71850
Vinicius Mendes de Godoi <vinicius.godoi@dinfor.uniceub>
2025-02-13 21:26:12 -0300

    origin master

--- Vinicius/sand-box/src/App.java
+++ Vinicius/sand-box/src/App.java
@@ -1,6 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Olá Orientação a Objeto");
+        System.out.println("Olá OO");
     }
 }
-
