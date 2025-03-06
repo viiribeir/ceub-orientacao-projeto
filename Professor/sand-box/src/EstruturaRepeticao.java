2085e3b76a9eae908b159fa641fba5ae9faf975d
Fernando Dias <fernando.dias@sicoob.com.br>
2025-03-06 19:39:09 -0300

    inicio aula revisão java

---
+++ Professor/sand-box/src/EstruturaRepeticao.java
@@ -0,0 +1,19 @@
+public class EstruturaRepeticao {
+    public static void main(String[] args) {
+
+        // i = i + 1 == i++
+        // for
+        for (int i = 1; i <= 5; i++) {
+            // System.out.println("valor: " + i);
+        }
+
+        // Proxima Aula Lista e for melhor
+
+        int contador = 0;
+        while (contador < 5) {
+            System.out.println("contador: " + contador);
+            contador++;
+        }
+
+    }
+}
---
+++ Professor/sand-box/src/VariaveisTipos.java
@@ -0,0 +1,22 @@
+public class VariaveisTipos {
+
+    public static void main(String[] args) {
+
+        // tipo Objeto
+        Integer numero = 10;
+        Double preco = 25.99;
+        char letra = 'A';
+        // tipo primitivo
+        boolean ativo = true;
+        Boolean ehFerias = null;
+
+        String texto = "Ola mundo!";
+        System.out.println(numero);
+        System.out.println(preco);
+        System.out.println(letra);
+        System.out.println(ativo);
+        System.out.println(ehFerias);
+        System.out.println(texto);
+
+    }
+}
