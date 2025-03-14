23996061f0b5d2121f1739907478c412620a5a98
Joao Victor Alves Rodrigues <joao.arodrigues@dinfor.uniceub>
2025-03-13 21:26:36 -0300

    atividade aula

---
+++ João Victor Alves/sand-box/src/atividades/atividadeAula.java
@@ -0,0 +1,19 @@
+package atividades;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class atividadeAula {
+
+    public static void main(String[] args) {
+        
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Digite o primeiro número :");
+        int num1 = scanner.nextInt();
+        System.out.println("Digite o segundo número :");
+        int num2 = scanner.nextInt();
+        System.out.println("A soma é " + (num1 + num2));
+
+    }
+
+}
