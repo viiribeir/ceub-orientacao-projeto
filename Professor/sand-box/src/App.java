38167ea968efbda82124aa24dd17ff1dc87cbca8
Fernando Dias <fernando.dias@sicoob.com.br>
2025-03-27 21:52:52 -0300

    aula de encapsulamento

--- Professor/sand-box/src/App.java
+++ Professor/sand-box/src/App.java
@@ -1,5 +1,9 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Olá OO");
+       
+        ContaBancaria contaNey = new ContaBancaria();
+        contaNey.setSaldo(1000);
+
+
     }
 }
---
+++ Professor/sand-box/src/ContaBancaria.java
@@ -0,0 +1,28 @@
+public class ContaBancaria {
+
+    private double saldo;
+
+
+    public void setSaldo(double valor) {
+        this.saldo = valor;
+    }
+
+    public double getSaldo() {
+        return this.saldo;
+    }
+
+
+    public void depositar(double valor){
+
+    }
+
+    private boolean sacar() {
+        return false;
+    }
+
+    public void imprimirSaldo(){
+        System.out.println("SALDO: " + saldo);
+    }
+}
+
+
