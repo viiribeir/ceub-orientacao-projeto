52c0c5ae9fe6100b58288978921e249bb5d7cd6c
Fernando Dias <fernando.dias@sicoob.com.br>
2025-06-12 20:38:57 -0300

    projeto final

--- Professor/exemplo_dispositivo/genericos_cast/exemplo_generics/projeto_final/calculadora_imc/src/App.java
+++ Professor/exemplo_dispositivo/genericos_cast/exemplo_generics/projeto_final/calculadora_imc/src/App.java
@@ -1,5 +1,7 @@
+import view.JanelaPrincipal;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+       new JanelaPrincipal();
     }
 }
